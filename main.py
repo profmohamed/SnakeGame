d36ae2f59bfc13b27fbea9d62db9195d35783fbc
profmohamed <mohamedsalem.ca@gmail.com>
2023-02-25 20:26:59 +0200

    snake game

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-import turtle import Screen, Turtle
+import time
+from turtle import Screen
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
-screen.title("My Snake Game)
+screen.title("My Snake Game")
+screen.tracer(0)
+
+sanke = Snake()
+
+screen.listen()
+screen.onkey(sanke.up, "Up")
+screen.onkey(sanke.down, "Down")
+screen.onkey(sanke.left, "Left")
+screen.onkey(sanke.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    sanke.move()
+
+
+screen.exitonclick()
